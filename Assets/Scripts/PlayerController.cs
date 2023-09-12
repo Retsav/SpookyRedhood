4d0a1f013c5cf959316f1328090343c19780b8d8
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-09-12 17:08:35 +0200

    Added collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRigidbody;
-    private PlayerInputActions playerInputActions;
-
-    [SerializeField] private float playerMovementSpeed = 5f;
-    private void Awake()
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private float moveSpeed = 7f;
+    private void Update()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
-        playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable();
-    }
-    
-    private void FixedUpdate()
-    {
-        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        playerRigidbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y)*playerMovementSpeed, ForceMode.Force);
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 1f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;   
+        }
     }
 }
